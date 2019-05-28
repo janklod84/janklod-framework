13f675bd0dd1b87e923c53103b07957c8430949d
janklod84 <jeanyao34@yahoo.com>
2019-05-28 10:12:58 +0500

    View refactoring

--- src/janklod/Routing/Controller.php
+++ src/janklod/Routing/Controller.php
@@ -11,88 +11,88 @@ use JK\Template\ViewAdapter;
 abstract class Controller 
 {
        
-     /**
-      * @var \JK\DI\ContainerInterface $app
-      * @var \JK\Template\View $view
-      * @var \JK\Http\Request $request
-      * @var string $layout
-      * @var array $data
-      * @var string $autoview 
-      * [ set automatically view by using controller and action ]
-     */
-     protected $app;
-     protected $view;
-     protected $request;
-     protected $layout;
-     protected $autoview = true;
+/**
+* @var \JK\DI\ContainerInterface $app
+* @var \JK\Template\View $view
+* @var \JK\Http\Request $request
+* @var string $layout
+* @var array $data
+* @var string $autoview 
+* [ set automatically view by using controller and action ]
+*/
+protected $app;
+protected $view;
+protected $request;
+protected $layout;
+protected $autoview = true;
 
-       
-     /**
-      * Constructor
-      * @param \JK\DI\ContainerInterface $app 
-      * @return void
-     */
-	   public function __construct($app)
-	   {
-           $this->app = $app;
-           $this->view = $app->view;
-	   }
+ 
+/**
+* Constructor
+* @param \JK\DI\ContainerInterface $app 
+* @return void
+*/
+public function __construct($app)
+{
+     $this->app = $app;
+     $this->view = $app->view;
+}
 
-       
-     /**
-      * Set data
-      * @param array $data 
-      * @return void
-     */
-     protected function set($data = [])
-     {
-          $this->view->setData($data);
-     }
-
-
-
-     /**
-      * View render
-      * @param string $view 
-      * @param array $data 
-      * @return mixed
-     */
-     protected function render($view, $data = [])
-     {
-           $this->view->setView($view);
-           $this->view->setData($data);
-           $this->view->setLayout(
-              $this->mapLayout()
-           );
-           (new ViewAdapter($this->view))->render();
-     }
-
-     /**
-      * map layout
-      * @return mixed
-     */
-     protected function mapLayout()
-     {
-         if($this->layout === false)
-         { return false; }
-         else{ return $this->layout ?: \Config::get('view.layout'); }
-     }
+ 
+/**
+* Set data
+* @param array $data 
+* @return void
+*/
+protected function set($data = [])
+{
+    $this->view->setData($data);
+}
 
 
-     /**
-      * Must to add later next functionnality
-      * Do something before calling action
-      * @return void
-     */
-     public function before() {}
+
+/**
+* View render
+* @param string $view 
+* @param array $data 
+* @return mixed
+*/
+protected function render($view, $data = [])
+{
+     $this->view->setView($view);
+     $this->view->setData($data);
+     $this->view->setLayout(
+        $this->mapLayout()
+     );
+     (new ViewAdapter($this->view))->render();
+}
+
+/**
+* map layout
+* @return mixed
+*/
+protected function mapLayout()
+{
+   if($this->layout === false)
+   { return false; }
+   else{ return $this->layout ?: \Config::get('view.layout'); }
+}
 
 
-     /**
-      * Must to add later next functionnality
-      * Do something after calling action
-      * @return void
-     */
-     public function after() {}
+/**
+* Must to add later next functionnality
+* Do something before calling action
+* @return void
+*/
+public function before() {}
+
+
+/**
+* Must to add later next functionnality
+* Do something after calling action
+* @return void
+*/
+public function after() {}
 
 
 
