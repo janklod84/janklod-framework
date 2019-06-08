3c48aa3a3eb55b85aa849f4f6b834785fa3765f7
janklod84 <jeanyao34@yahoo.com>
2019-06-09 01:22:26 +0500

    Change application structure

--- src/janklod/Console/IO/InputArgv.php
+++ src/janklod/Console/IO/InputArgv.php
@@ -6,16 +6,43 @@ namespace JK\Console\IO;
 */ 
 class InputArgv implements InputInterface
 {
-      public function argument($key=null)
+
+/**
+ * Get input argument from console
+ * @param null|string $key 
+ * @return mixed
+*/
+public function argument($key=null)
+{   
+	  if($this->is_cli())
 	  {
-	  	  if(isset($_SERVER['argv']))
+	  	  $arguments = $_SERVER['argv'];
+	  	  if($key)
 	  	  {
-	  	  	  $arguments = $_SERVER['argv'];
-	  	  	  if($key)
-	  	  	  {
-	  	  	  	 $arguments = $arguments[$key] ?? '';
-	  	  	  }
-	  	  	  return $arguments;
+	  	  	 $arguments = $arguments[$key] ?? '';
 	  	  }
+	  	  return $arguments;
 	  }
+}
+      
+/**
+* Give count of parses input
+* @return int
+*/
+public function count()
+{
+   return $_SERVER['argc'];
+}
+
+
+/**
+ * Determine if has mode CLI [ Command Line Interface ]
+ * @return bool
+*/
+public function is_cli()
+{
+	return (php_sapi_name() != 'cli')
+	       || $_SERVER['argc'] > 0;
+}
+
 }
