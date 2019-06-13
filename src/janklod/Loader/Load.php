229a864f75fffd9b0c5384aec7c70c69af557028
janklod84 <jeanyao34@yahoo.com>
2019-06-13 17:14:48 +0500

    RouteParam

--- src/janklod/Loader/Load.php
+++ src/janklod/Loader/Load.php
@@ -14,12 +14,9 @@ class Load
 {
       
 /**
-* @var \JK\Container\ContainerInterface $app
-* @var  
+* @var ContainerInterface $app
 */
 private $app;
-private $models = [];
-
 
 
 /**
@@ -55,73 +52,8 @@ public function callAction($callback, $matches=[])
 */
 public function model($model)
 {
-    $model = $this->getModelName($model) ?: new \stdClass();
-    if(!$this->hasModel($model))
-    {
-        $this->addModel($model);
-    }
-    return $this->getModel($model); 
+    $loader = new ModelLoader($this->app);
+    return $loader->model($model);
 }  
-
-
-/**
-* Determine if the given 
-* class exists in the models container
-* 
-* @param string $model
-* @return bool
-*/
-public function hasModel($model)
-{
-     return array_key_exists($model, $this->models);
-}
-
-
-/**
- * Create new object for the given controller and store it
- * in models container
- *
- * @param string $model
- * @return void
-*/
-private function addModel($model)
-{
-   $object = new $model($this->app);
-   $this->models[$model] = $object;
-}
-
-
-
-/**
-* Get the model  object
-*
-* @param string $model
-* @return object
-*/
-private function getModel($model)
-{
-    return $this->models[$model];
-}
-
-
-/**
-* Get the full class name for the given model
-*
-* @param  string $name
-* @return string
-*/
-private function getModelName($name)
-{
-   $model = $this->getModule('\\app\\models', $name);
-   if(!class_exists($model))
-   {
-       exit(sprintf(
-        'Sorry class Model [ <b>%s</b> ] does not exist!', 
-        $model)
-       ); 
-   }
-   return $model;
-}
-
 
 }
--- src/janklod/Loader/Loaders/ModelLoader.php
+++ src/janklod/Loader/Loaders/ModelLoader.php
@@ -8,5 +8,86 @@ namespace JK\Loader\Loaders;
 class ModelLoader extends CustomLoader
 {
 
+/**
+ * @var array $models
+*/
+private $models = [];
+
+/**
+* Load model
+* 
+* @param string $name 
+* @return object
+*/
+public function model($name)
+{
+    $model = $this->getModelName($model) ?: new \stdClass();
+    if(!$this->hasModel($model))
+    {
+        $this->addModel($model);
+    }
+    return $this->getModel($model); 
+}
+
+
+
+/**
+* Determine if the given 
+* class exists in the models container
+* 
+* @param string $model
+* @return bool
+*/
+private function hasModel($model)
+{
+     return array_key_exists($model, $this->models);
+}
+
+
+/**
+ * Create new object for the given controller and store it
+ * in models container
+ *
+ * @param string $model
+ * @return void
+*/
+private function addModel($model)
+{
+   $object = new $model($this->app);
+   $this->models[$model] = $object;
+}
+
+
+
+/**
+* Get the model  object
+*
+* @param string $model
+* @return object
+*/
+private function getModel($model)
+{
+    return $this->models[$model];
+}
+
+
+/**
+* Get the full class name for the given model
+*
+* @param  string $name
+* @return string
+*/
+private function getModelName($name)
+{
+   $model = $this->module('\\app\\models', $name);
+   if(!class_exists($model))
+   {
+       exit(sprintf(
+        'Sorry class Model [ <b>%s</b> ] does not exist!', 
+        $model)
+       ); 
+   }
+   return $model;
+}
 
 }
